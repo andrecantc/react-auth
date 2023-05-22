baf94c01434a84ba9ba7d15df7c3c2da1a1e19b2
André Gaudet <89323446+andrecantc@users.noreply.github.com>
2023-05-22 16:37:32 +0200

    Add files via upload

---
+++ auth/src/App.js
@@ -0,0 +1,19 @@
+import Register from './Register';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+
+
+function App() {
+	return (
+		<main className="App">
+			<Router>
+				<Routes>
+					<Route path="/" exact element={<Register />} />
+					<Route path="/login" element={<Login />} />
+				</Routes>
+			</Router>
+		</main>
+	);
+}
+
+export default App;
